f72161d66dae8bfa9379b44242174183a67f7b9a
RVSVTMNTS <rvsvetements@gmail.com>
2023-04-28 15:39:24 +0200

    timer

--- js/scripts.js
+++ js/scripts.js
@@ -51,3 +51,29 @@ document.addEventListener('DOMContentLoaded', function () {
         epilepsyWarningModal.style.display = 'none';
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var countdownElement = document.getElementById('countdown');
+    var countdownTextElement = document.getElementById('countdownText');
+    var countdownDate = new Date('May 15, 2023 00:00:00 UTC+2').getTime();
+
+    function updateCountdown() {
+        var now = new Date().getTime();
+        var timeLeft = countdownDate - now;
+
+        var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        countdownTextElement.textContent = 'Project #2 - SOON :  ';
+        countdownElement.textContent = days + 'j ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
+
+        if (timeLeft < 0) {
+            clearInterval(countdownInterval);
+            countdownElement.textContent = 'Terminé';
+        }
+    }
+
+    var countdownInterval = setInterval(updateCountdown, 1000);
+});
